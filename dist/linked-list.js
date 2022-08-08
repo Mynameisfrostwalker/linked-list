865aff2b53392cbfb1d298a13042d17ae7fe0e55
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-08-08 09:01:43 +0100

    Create linked list class

--- dist/linked-list.js
+++ dist/linked-list.js
@@ -1,2 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var LinkedList = /** @class */ (function () {
+    function LinkedList(list) {
+        if (list === void 0) { list = null; }
+        this.list = list;
+    }
+    return LinkedList;
+}());
 //# sourceMappingURL=linked-list.js.map
--- scripts/linked-list.ts
+++ scripts/linked-list.ts
@@ -1,1 +1,10 @@
+import ListNode from "./linked-node";
 
+
+class LinkedList {
+    list: ListNode | null;
+    
+    constructor(list: ListNode | null = null) {
+        this.list = list;
+    }
+}
